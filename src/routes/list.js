4a978f5e49fb2406bd6c2a6836245417a3ca1ee2
shunichiro <5934746+syunduel@users.noreply.github.com>
2022-06-12 15:27:44 +0900

    NFTを取得できたが表示ができない

--- src/routes/list.js
+++ src/routes/list.js
@@ -6,7 +6,14 @@ import { Link } from "react-router-dom";
 
 export default function List() {
 
+    // Moralis
+    console.log('useMoralis start');
     const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+    console.log('useMoralis end');
+
+    console.log('useMoralisWeb3Api start');
+    const Web3Api = useMoralisWeb3Api();
+    console.log('useMoralisWeb3Api end');
 
     useEffect(() => {
         console.log("isAuthenticated : " + isAuthenticated);
@@ -19,30 +26,88 @@ export default function List() {
             // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isAuthenticated]);
 
-    // Moralis
-    const Web3Api = useMoralisWeb3Api();
-
     // LAG
     const baseNftAddress = "0x9c99d7f09d4a7e23ea4e36aec4cb590c5bbdb0e2";
-    const baseNftImgHash = "QmWsNgLwBLiqGpUrhUS48ejRFAgUA92ChF6Xo45uMKWnmQ";
+    const baseNftImgHash = "https://ipfs.moralis.io:2053/ipfs/QmWsNgLwBLiqGpUrhUS48ejRFAgUA92ChF6Xo45uMKWnmQ/";
     // const baseNftChain = "eth";
     const tokenIdDigit = 4;
 
-    var nfts = null;
-
     // get NFTs from connected wallet address
-    async function dispNFTs() {
+    const dispNFTs = () => {
+        if (isAuthenticated) {
+            try {
+                let nfts = getNFTs();
+                console.log("dispNFTs nfts");
+                console.log(nfts);
+                return nfts;
+            } catch(e) {
+                console.log(e);
+            }
+        } else {
+            return <div id="hogehoge"></div>;
+        }
 
-    getNFTs();
-    // renderNFTs(nfts);
     }
 
     const getNFTs = () => {
+        // get NFTs for current user on Mainnet
+        let nfts = null;
+        Web3Api.account.getNFTs()
+        .then(nfts => {
+            nfts = renderNFTs(nfts);
+            console.log("getNFTs nfts");
+            console.log(nfts);
+            return nfts;
+        });
+    }
 
-    // get NFTs for current user on Mainnet
-    const userEthNFTs = Web3Api.account.getNFTs();
-    console.log(userEthNFTs);
+    const renderNFTs = (nfts) => {
 
+        try {
+            console.log("renderNFTs nfts");
+            console.log(nfts);
+            console.log("renderNFTs nfts.result");
+            console.log(nfts.result);
+        } catch (e) {
+            //
+        }
+
+        if (nfts == null || nfts.result == undefined) {
+            return;
+        }
+
+        const items = [];
+
+        for (var i in nfts.result) {
+            let row = nfts.result[i];
+            // console.log(row.token_id);
+
+            if (row.token_address == baseNftAddress) {
+                var nowTokenId = zeroPadding(row.token_id, tokenIdDigit);
+                // console.log(`${row.token_id}`);
+                items.push(
+                    <CardNFT
+                        CardNFT={{
+                            token_address: row.token_address,
+                            collection_name: 'Love Addicted Girls',
+                            token_id: nowTokenId,
+                            name: row.name,
+                            image: baseNftImgHash + nowTokenId + '.png',
+                        }}
+                        height="368px"
+                        width="300px"
+                        margin="10px 10px 10px 10px" />
+                );
+            }
+        }
+        console.log("items");
+        console.log(items);
+        return items;
+
+    }
+
+    function zeroPadding(token_id, zeroPaddingLength) {
+        return ('000000000' + token_id).slice( zeroPaddingLength * -1 );
     }
 
     return (
@@ -51,6 +116,7 @@ export default function List() {
                 <p className="catch-copy">Let's dress up your NFT</p>
                 <p>You can change your NFT clothes. First, select the NFT you want to dress up.</p>
             </div>
+            {dispNFTs()}
             <Link to="/dressup">
                 <CardNFT
                     CardNFT={{
