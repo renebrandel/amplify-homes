e78af5ebbc30ce7eeea17c1e03d513c2865d8557
‘Tomura <tomura@prime-concept.co.jp>
2023-10-31 03:34:21 +0900

    first commit

---
+++ ui-components/HomeCreateForm.d.ts
@@ -0,0 +1,54 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { GridProps, TextFieldProps } from "@aws-amplify/ui-react";
+export declare type EscapeHatchProps = {
+    [elementHierarchy: string]: Record<string, unknown>;
+} | null;
+export declare type VariantValues = {
+    [key: string]: string;
+};
+export declare type Variant = {
+    variantValues: VariantValues;
+    overrides: EscapeHatchProps;
+};
+export declare type ValidationResponse = {
+    hasError: boolean;
+    errorMessage?: string;
+};
+export declare type ValidationFunction<T> = (value: T, validationResponse: ValidationResponse) => ValidationResponse | Promise<ValidationResponse>;
+export declare type HomeCreateFormInputValues = {
+    address?: string;
+    image_url?: string;
+    age?: number;
+    name?: string;
+};
+export declare type HomeCreateFormValidationValues = {
+    address?: ValidationFunction<string>;
+    image_url?: ValidationFunction<string>;
+    age?: ValidationFunction<number>;
+    name?: ValidationFunction<string>;
+};
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type HomeCreateFormOverridesProps = {
+    HomeCreateFormGrid?: PrimitiveOverrideProps<GridProps>;
+    address?: PrimitiveOverrideProps<TextFieldProps>;
+    image_url?: PrimitiveOverrideProps<TextFieldProps>;
+    age?: PrimitiveOverrideProps<TextFieldProps>;
+    name?: PrimitiveOverrideProps<TextFieldProps>;
+} & EscapeHatchProps;
+export declare type HomeCreateFormProps = React.PropsWithChildren<{
+    overrides?: HomeCreateFormOverridesProps | undefined | null;
+} & {
+    clearOnSuccess?: boolean;
+    onSubmit?: (fields: HomeCreateFormInputValues) => HomeCreateFormInputValues;
+    onSuccess?: (fields: HomeCreateFormInputValues) => void;
+    onError?: (fields: HomeCreateFormInputValues, errorMessage: string) => void;
+    onChange?: (fields: HomeCreateFormInputValues) => HomeCreateFormInputValues;
+    onValidate?: HomeCreateFormValidationValues;
+} & React.CSSProperties>;
+export default function HomeCreateForm(props: HomeCreateFormProps): React.ReactElement;
---
+++ ui-components/HomeUpdateForm.jsx
@@ -0,0 +1,283 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { Button, Flex, Grid, TextField } from "@aws-amplify/ui-react";
+import { Home } from "../models";
+import { fetchByPath, getOverrideProps, validateField } from "./utils";
+import { DataStore } from "aws-amplify";
+export default function HomeUpdateForm(props) {
+  const {
+    id: idProp,
+    home: homeModelProp,
+    onSuccess,
+    onError,
+    onSubmit,
+    onValidate,
+    onChange,
+    overrides,
+    ...rest
+  } = props;
+  const initialValues = {
+    address: "",
+    image_url: "",
+    age: "",
+    name: "",
+  };
+  const [address, setAddress] = React.useState(initialValues.address);
+  const [image_url, setImage_url] = React.useState(initialValues.image_url);
+  const [age, setAge] = React.useState(initialValues.age);
+  const [name, setName] = React.useState(initialValues.name);
+  const [errors, setErrors] = React.useState({});
+  const resetStateValues = () => {
+    const cleanValues = homeRecord
+      ? { ...initialValues, ...homeRecord }
+      : initialValues;
+    setAddress(cleanValues.address);
+    setImage_url(cleanValues.image_url);
+    setAge(cleanValues.age);
+    setName(cleanValues.name);
+    setErrors({});
+  };
+  const [homeRecord, setHomeRecord] = React.useState(homeModelProp);
+  React.useEffect(() => {
+    const queryData = async () => {
+      const record = idProp
+        ? await DataStore.query(Home, idProp)
+        : homeModelProp;
+      setHomeRecord(record);
+    };
+    queryData();
+  }, [idProp, homeModelProp]);
+  React.useEffect(resetStateValues, [homeRecord]);
+  const validations = {
+    address: [],
+    image_url: [],
+    age: [],
+    name: [],
+  };
+  const runValidationTasks = async (
+    fieldName,
+    currentValue,
+    getDisplayValue
+  ) => {
+    const value =
+      currentValue && getDisplayValue
+        ? getDisplayValue(currentValue)
+        : currentValue;
+    let validationResponse = validateField(value, validations[fieldName]);
+    const customValidator = fetchByPath(onValidate, fieldName);
+    if (customValidator) {
+      validationResponse = await customValidator(value, validationResponse);
+    }
+    setErrors((errors) => ({ ...errors, [fieldName]: validationResponse }));
+    return validationResponse;
+  };
+  return (
+    <Grid
+      as="form"
+      rowGap="15px"
+      columnGap="15px"
+      padding="20px"
+      onSubmit={async (event) => {
+        event.preventDefault();
+        let modelFields = {
+          address,
+          image_url,
+          age,
+          name,
+        };
+        const validationResponses = await Promise.all(
+          Object.keys(validations).reduce((promises, fieldName) => {
+            if (Array.isArray(modelFields[fieldName])) {
+              promises.push(
+                ...modelFields[fieldName].map((item) =>
+                  runValidationTasks(fieldName, item)
+                )
+              );
+              return promises;
+            }
+            promises.push(
+              runValidationTasks(fieldName, modelFields[fieldName])
+            );
+            return promises;
+          }, [])
+        );
+        if (validationResponses.some((r) => r.hasError)) {
+          return;
+        }
+        if (onSubmit) {
+          modelFields = onSubmit(modelFields);
+        }
+        try {
+          Object.entries(modelFields).forEach(([key, value]) => {
+            if (typeof value === "string" && value === "") {
+              modelFields[key] = null;
+            }
+          });
+          await DataStore.save(
+            Home.copyOf(homeRecord, (updated) => {
+              Object.assign(updated, modelFields);
+            })
+          );
+          if (onSuccess) {
+            onSuccess(modelFields);
+          }
+        } catch (err) {
+          if (onError) {
+            onError(modelFields, err.message);
+          }
+        }
+      }}
+      {...getOverrideProps(overrides, "HomeUpdateForm")}
+      {...rest}
+    >
+      <TextField
+        label="Address"
+        isRequired={false}
+        isReadOnly={false}
+        value={address}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              address: value,
+              image_url,
+              age,
+              name,
+            };
+            const result = onChange(modelFields);
+            value = result?.address ?? value;
+          }
+          if (errors.address?.hasError) {
+            runValidationTasks("address", value);
+          }
+          setAddress(value);
+        }}
+        onBlur={() => runValidationTasks("address", address)}
+        errorMessage={errors.address?.errorMessage}
+        hasError={errors.address?.hasError}
+        {...getOverrideProps(overrides, "address")}
+      ></TextField>
+      <TextField
+        label="Image url"
+        isRequired={false}
+        isReadOnly={false}
+        value={image_url}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              address,
+              image_url: value,
+              age,
+              name,
+            };
+            const result = onChange(modelFields);
+            value = result?.image_url ?? value;
+          }
+          if (errors.image_url?.hasError) {
+            runValidationTasks("image_url", value);
+          }
+          setImage_url(value);
+        }}
+        onBlur={() => runValidationTasks("image_url", image_url)}
+        errorMessage={errors.image_url?.errorMessage}
+        hasError={errors.image_url?.hasError}
+        {...getOverrideProps(overrides, "image_url")}
+      ></TextField>
+      <TextField
+        label="Age"
+        isRequired={false}
+        isReadOnly={false}
+        type="number"
+        step="any"
+        value={age}
+        onChange={(e) => {
+          let value = isNaN(parseInt(e.target.value))
+            ? e.target.value
+            : parseInt(e.target.value);
+          if (onChange) {
+            const modelFields = {
+              address,
+              image_url,
+              age: value,
+              name,
+            };
+            const result = onChange(modelFields);
+            value = result?.age ?? value;
+          }
+          if (errors.age?.hasError) {
+            runValidationTasks("age", value);
+          }
+          setAge(value);
+        }}
+        onBlur={() => runValidationTasks("age", age)}
+        errorMessage={errors.age?.errorMessage}
+        hasError={errors.age?.hasError}
+        {...getOverrideProps(overrides, "age")}
+      ></TextField>
+      <TextField
+        label="Name"
+        isRequired={false}
+        isReadOnly={false}
+        value={name}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              address,
+              image_url,
+              age,
+              name: value,
+            };
+            const result = onChange(modelFields);
+            value = result?.name ?? value;
+          }
+          if (errors.name?.hasError) {
+            runValidationTasks("name", value);
+          }
+          setName(value);
+        }}
+        onBlur={() => runValidationTasks("name", name)}
+        errorMessage={errors.name?.errorMessage}
+        hasError={errors.name?.hasError}
+        {...getOverrideProps(overrides, "name")}
+      ></TextField>
+      <Flex
+        justifyContent="space-between"
+        {...getOverrideProps(overrides, "CTAFlex")}
+      >
+        <Button
+          children="Reset"
+          type="reset"
+          onClick={(event) => {
+            event.preventDefault();
+            resetStateValues();
+          }}
+          isDisabled={!(idProp || homeModelProp)}
+          {...getOverrideProps(overrides, "ResetButton")}
+        ></Button>
+        <Flex
+          gap="15px"
+          {...getOverrideProps(overrides, "RightAlignCTASubFlex")}
+        >
+          <Button
+            children="Submit"
+            type="submit"
+            variation="primary"
+            isDisabled={
+              !(idProp || homeModelProp) ||
+              Object.values(errors).some((e) => e?.hasError)
+            }
+            {...getOverrideProps(overrides, "SubmitButton")}
+          ></Button>
+        </Flex>
+      </Flex>
+    </Grid>
+  );
+}
